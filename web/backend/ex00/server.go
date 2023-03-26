4083407292280b883a865204dd34413e820deb71
Ricardo <rico42nabo@gmail.com>
2023-03-27 00:11:17 +0100

    finish ex00 with go

---
+++ web/backend/ex00/server.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func main() {
+    fmt.Printf("Starting server at port 3000\n")
+    if err := http.ListenAndServe(":3000", nil); err != nil {
+        log.Fatal(err)
+    }
+}
